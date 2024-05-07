edb53d755595e065636e1ecdf97acfa275dd1b29
Rahul-rk123 <Ry6817319@gmail.com>
2024-05-07 12:43:26 +0530

    ex1

---
+++ ex1.java
@@ -0,0 +1,19 @@
+package VML_repo2;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Scanner sd = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str = sd.next();
+        System.out.println(fir(str));
+        sd.close();
+    }
+    public static String fir(String s){
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        s = sb.toString();
+        return s;
+    }
+}
